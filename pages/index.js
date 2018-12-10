f8dfb3e8b873d5ddfd2cdec9febeb2afacdf8549
Ondřej Tůma <ondr@centrum.cz>
2018-12-10 23:58:14 +0100

    server.js, index page, _document.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Index = () => (
+    <div>
+        <Head>
+            <title>{`Sing along`}</title>
+        </Head>
+        <h1>ahoj</h1>
+    </div>
+);
+
+export default Index;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const APP_PORT = 3000;
+
+app.prepare()
+    .then(() => {
+        const server = express();
+
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        });
+
+        server.listen(APP_PORT, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`> Ready on http://localhost:${APP_PORT}`);
+        });
+    })
+    .catch((ex) => {
+        console.error(ex.stack);
+        process.exit(1)
+    });
