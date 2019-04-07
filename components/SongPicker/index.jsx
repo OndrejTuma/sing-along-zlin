f6da42f6cab9c5e848b4405838a0dae3071911ca
Ondřej Tůma <ondr@centrum.cz>
2019-04-07 22:18:35 +0200

    sorting songs in songpicker alphabetically

--- components/SongPicker/index.jsx
+++ components/SongPicker/index.jsx
@@ -33,7 +33,11 @@ function SongPicker({name}, ref) {
             <Input name={name} type={'hidden'} value={pickedSongIds}/>
             <label>Vyber písničku:</label>
             <ul>
-                {[...songs.values()].map(({_id: id, title}) => (
+                {[...songs.values()].sort(function(a, b){
+                    if(a.title < b.title) { return -1; }
+                    if(a.title > b.title) { return 1; }
+                    return 0;
+                }).map(({_id: id, title}) => (
                     <li
                         key={id}
                         onClick={() => handlePickSong(id)}
