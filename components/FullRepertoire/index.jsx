0045afac3f96c6a3fe1572db8e7218fbbc5ecadc
Ondřej Tůma <ondr@centrum.cz>
2019-10-13 20:30:46 +0200

    matching order of songs in section in index.js page

--- components/FullRepertoire/index.jsx
+++ components/FullRepertoire/index.jsx
@@ -9,16 +9,18 @@ import globalStyles from 'Sass/global.scss';
 import styles from './styles.scss';
 
 function FullRepertoire({repertoire, sections, songs}) {
+    const songsMap = new Map(songs.map(song => ([song._id, song])));
+
     return (
         <div className={classNames(globalStyles.wrapper, styles.wrapper)}>
             <h1>{repertoire.title}</h1>
             <RepertoireNavigation sections={sections} songs={songs}/>
             {sections.map(({_id, title: sectionTitle, songs: sectionSongs}) => {
-                const songsList = songs.filter(song => sectionSongs.indexOf(song._id) > -1);
+                const songsList = sectionSongs.map(id => songsMap.get(id));
 
                 return (
                     <div className={styles.section} key={_id} id={sectionTitle}>
-                        {songsList.map(({_id: id, title, text}, index) => (
+                        {songsList.filter(song => song).map(({_id: id, title, text}, index) => (
                             <div key={id} className={styles.song}>
                                 <h2>{index === 0 && <small className={styles.sectionName}>{sectionTitle}:</small> }{title}</h2>
                                 <div dangerouslySetInnerHTML={{__html: getHTMLFromStringifiedState(text)}}/>
